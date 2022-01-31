0d81a25d795568ba152c124c346eac4a8a60d3ca
Melenium2 <osumelenium@gmail.com>
2022-01-31 09:38:09 +0500

    feat(table): add draft for table

---
+++ internal/table/table.go
@@ -0,0 +1,49 @@
+package table
+
+import (
+	"github.com/Melenium2/kademlia"
+	"github.com/Melenium2/kademlia/internal/table/node"
+)
+
+const (
+	// Alpha is a small number representing the degree of parallelism in network calls, usually 3
+	Alpha = 3
+	// BucketSize is Kademlia single bucket size.
+	BucketSize = 16
+
+	hashBits          = len(kademlia.ID{}) * 8 // Length of hash in bits. Now this is length of SHA-1, 160 bits.
+	nBuckets          = hashBits / 15          // Number of buckets.
+	bucketMinDistance = hashBits - nBuckets    // Log distance of the closest bucket.
+
+)
+
+type BucketPersistenceStore interface {
+}
+
+type Config struct {
+	BootNodes []*kademlia.Node
+}
+
+type bucket struct {
+	entries      []*node.Node // live entries, sorted by time of last ping.
+	replacements []*node.Node // if one of entries is dies, then we chose one from this list, if it is still available.
+}
+
+type Table struct {
+	store   BucketPersistenceStore
+	buckets []*bucket
+	self    *node.Node
+}
+
+func NewTable(cfg *Config, db BucketPersistenceStore, kadenode *kademlia.Node) *Table {
+	return &Table{
+		store: db,
+		self:  node.WrapNode(kadenode),
+	}
+}
+
+func (t *Table) Discover() {}
+
+// TODO we can start implement protocol from doRefresh function
+// http://xlattice.sourceforge.net/components/protocol/kademlia/specs.html#implementation
+// https://en.wikipedia.org/wiki/Kademlia
