f670ad33654526b9fe2e9b6179a38b5f941cd57e
Melenium2 <osumelenium@gmail.com>
2022-02-02 09:40:08 +0500

    test(lookup_test): add new test

--- internal/table/lookup_test.go
+++ internal/table/lookup_test.go
@@ -1,6 +1,7 @@
 package table
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/Melenium2/kademlia"
@@ -62,4 +63,32 @@ func TestScan_Should_call_Find_func_which_return_some_error(t *testing.T) {
 		assert.Error(t, err)
 		assert.Equal(t, ErrEmptyBootstrapNodes, err)
 	}
+}
+
+func TestConsume_Should_save_all_incoming_nodes_from_provided_channel_also_nodes_should_be_sorted_by_id(t *testing.T) {
+	var (
+		self     = randomNode()
+		expected = []*node.Node{randomNode(), randomNode(), randomNode()}
+		resCh    = make(chan []*node.Node, Alpha)
+		errCh    = make(chan error, 1)
+	)
+	l := newLookup(lookupConfig{}, nil, self)
+
+	resCh <- expected
+
+	err := l.consume(resCh, errCh)
+	assert.NoError(t, err)
+
+	for i := 0; i < len(expected); i++ {
+		_, ok := l.seenNodes[expected[i].ID()]
+		assert.True(t, ok)
+	}
+
+	assert.Equal(t, len(expected), len(l.resultNodes.nodes))
+
+	sort.Slice(expected, func(i, j int) bool {
+		return node.DistanceCmp(self.ID(), expected[i].ID(), expected[j].ID()) < 0
+	})
+
+	assert.Equal(t, expected, l.resultNodes.Nodes())
 }
