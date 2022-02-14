e671b06828db38c6993e57e9cee6be799f1cca6e
melenium2 <osumelenium@gmail.com>
2022-02-14 09:17:35 +0500

    feat(packets): add marshal and unmarshal functions, also add some fields to Ping and Pong message

--- internal/conn/packets.go
+++ internal/conn/packets.go
@@ -1,16 +1,28 @@
 package conn
 
+import (
+	"crypto/rand"
+	"encoding/json"
+	"net"
+)
+
 const (
 	PingMessage byte = iota + 1
 	PongMessage
 )
 
 type Packet interface {
+	SetRequestID(id []byte)
 	Name() string
 	IAm() byte
 }
 
 type Ping struct {
+	ReqID []byte `json:"req_id,omitempty"`
+}
+
+func (p *Ping) SetRequestID(id []byte) {
+	p.ReqID = id
 }
 
 func (p *Ping) Name() string {
@@ -22,6 +34,13 @@ func (p *Ping) IAm() byte {
 }
 
 type Pong struct {
+	ReqID []byte `json:"req_id,omitempty"`
+	IP    net.IP `json:"ip,omitempty"`
+	Port  uint16 `json:"port,omitempty"`
+}
+
+func (p *Pong) SetRequestID(id []byte) {
+	p.ReqID = id
 }
 
 func (p *Pong) Name() string {
@@ -32,10 +51,32 @@ func (p *Pong) IAm() byte {
 	return PongMessage
 }
 
-func makePing() Ping {
-	return Ping{}
+func GenerateReqID() []byte {
+	reqID := make([]byte, 8)
+
+	_, _ = rand.Read(reqID)
+
+	return reqID
 }
 
-func Encode(req Ping) []byte {
-	return nil
+func Marshal(packet Packet) []byte {
+	raw, _ := json.Marshal(packet)
+
+	return raw
+}
+
+func Unmarshal(bodytype byte, raw []byte) (Packet, error) {
+	var packet Packet
+	switch bodytype {
+	case PingMessage:
+		packet = &Ping{}
+	case PongMessage:
+		packet = &Pong{}
+	}
+
+	if err := json.Unmarshal(raw, &packet); err != nil {
+		return nil, err
+	}
+
+	return packet, nil
 }
