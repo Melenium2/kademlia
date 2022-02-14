244b9dda2fae564cac35611f5eb57a6b34d19821
melenium2 <osumelenium@gmail.com>
2022-02-14 09:18:15 +0500

    test(packets_test): add tests

---
+++ internal/conn/packets_test.go
@@ -0,0 +1,43 @@
+package conn_test
+
+import (
+	"net"
+	"testing"
+
+	"github.com/Melenium2/kademlia/internal/conn"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	ping              = &conn.Ping{ReqID: []byte("13123123")}
+	expectedPingBytes = []byte(`{"req_id":"MTMxMjMxMjM="}`)
+
+	pong = &conn.Pong{
+		ReqID: []byte("13123123"),
+		IP:    net.IPv4(1, 1, 1, 1),
+		Port:  5222,
+	}
+	expectedPongBytes = []byte(`{"req_id":"MTMxMjMxMjM=","ip":"1.1.1.1","port":5222}`)
+)
+
+func TestMarshal_Should_marshal_ping_to_valid_bytes(t *testing.T) {
+	res := conn.Marshal(ping)
+	assert.Equal(t, expectedPingBytes, res)
+}
+
+func TestUnmarshal_Should_unmarshal_bytes_to_valid_ping_struct(t *testing.T) {
+	newPing, err := conn.Unmarshal(conn.PingMessage, expectedPingBytes)
+	assert.NoError(t, err)
+	assert.Equal(t, ping, newPing)
+}
+
+func TestMarshal_Should_marshal_pong_to_valid_bytes(t *testing.T) {
+	res := conn.Marshal(pong)
+	assert.Equal(t, expectedPongBytes, res)
+}
+
+func TestUnmarshal_Should_unmarshal_bytes_to_valid_pong_struct(t *testing.T) {
+	newPong, err := conn.Unmarshal(conn.PongMessage, expectedPongBytes)
+	assert.NoError(t, err)
+	assert.Equal(t, pong, newPong)
+}
