da75040cb66f149ace49c87491cd77589a5e96c1
melenium2 <osumelenium@gmail.com>
2022-02-18 09:42:19 +0500

    feat(erx): add new err

--- internal/conn/erx.go
+++ internal/conn/erx.go
@@ -2,4 +2,7 @@ package conn
 
 import "errors"
 
-var ErrWrongMessageType = errors.New("got wrong message type")
+var (
+	ErrWrongMessageType = errors.New("got wrong message type")
+	ErrEmptyMessage     = errors.New("empty raw message")
+)
