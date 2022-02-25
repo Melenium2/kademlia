889b9d4fb27ecfd11c086a56db03cfb02684d556
melenium2 <osumelenium@gmail.com>
2022-02-25 10:30:05 +0500

    refactor(ordered_nodes): move ordered nodes to different file

--- internal/table/lookup.go
+++ internal/table/lookup.go
@@ -2,59 +2,11 @@ package table
 
 import (
 	"net"
-	"sort"
 
 	"github.com/Melenium2/kademlia"
 	"github.com/Melenium2/kademlia/internal/table/node"
 	"github.com/Melenium2/kademlia/pkg/logger"
 )
-
-// orderedNodes stores Node's by their distance from our self Node.
-//
-// Node list has limited count. Limit is set by constructor.
-type orderedNodes struct {
-	// list of nodes.
-	nodes []*node.Node
-	// maximum count of stored nodes.
-	nodesLimit int
-	// pointer to our Node.
-	self *node.Node
-}
-
-// newOrderedNodes create new instance of Node's storage.
-func newOrderedNodes(self *node.Node, limit int) orderedNodes {
-	return orderedNodes{
-		self:       self,
-		nodes:      make([]*node.Node, 0, limit),
-		nodesLimit: limit,
-	}
-}
-
-// Add new Node to position in Node's slice by distance between
-// provided Node and self Node.
-func (on *orderedNodes) Add(newNode *node.Node) {
-	index := sort.Search(len(on.nodes), func(i int) bool {
-		return node.DistanceCmp(on.self.ID(), on.nodes[i].ID(), newNode.ID()) > 0
-	})
-
-	// we add new item to the end of list if limit not reached.
-	if len(on.nodes) < on.nodesLimit {
-		on.nodes = append(on.nodes, newNode)
-	}
-
-	// if index less than length of nodes we insert it to the
-	// position 'index', otherwise, 'index' equals to last element
-	// of list and this is means we already set new node to
-	// right position.
-	if index < len(on.nodes) {
-		copy(on.nodes[index+1:], on.nodes[index:])
-		on.nodes[index] = newNode
-	}
-}
-
-func (on *orderedNodes) Nodes() []*node.Node {
-	return on.nodes
-}
 
 // Finder is interface which provide functionality to network find possibility.
 // Finder try to connect to Node with settings provided by the arguments
