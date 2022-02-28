ae290ea057a3465eeadd7e2e4901d66f4dfe76fa
melenium2 <osumelenium@gmail.com>
2022-02-28 09:04:58 +0500

    feat(erx): add new error

--- internal/conn/erx.go
+++ internal/conn/erx.go
@@ -3,7 +3,8 @@ package conn
 import "errors"
 
 var (
-	ErrWrongMessageType = errors.New("got wrong message type")
-	ErrEmptyMessage     = errors.New("empty raw message")
-	ErrValidate         = errors.New("error while validating incoming packet")
+	ErrWrongMessageType    = errors.New("got wrong message type")
+	ErrEmptyMessage        = errors.New("empty raw message")
+	ErrValidate            = errors.New("error while validating incoming packet")
+	ErrNotMatchAnyDistance = errors.New("does not match any request distance")
 )
