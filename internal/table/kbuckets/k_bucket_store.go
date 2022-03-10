5dd2ff64fb5286e4031045b36c3dff93818e30d8
melenium2 <osumelenium@gmail.com>
2022-03-10 09:28:03 +0500

    feat(k_bucket_store): add new func

--- internal/table/kbuckets/k_bucket_store.go
+++ internal/table/kbuckets/k_bucket_store.go
@@ -7,15 +7,21 @@ type Bucket struct {
 }
 
 type KBuckets struct {
+	self        *node.Node
 	buckets     []*Bucket
 	minDistance int
 }
 
-func New(n, minDist int) *KBuckets {
+func New(self *node.Node, n, minDist int) *KBuckets {
 	return &KBuckets{
+		self:        self,
 		buckets:     make([]*Bucket, n),
 		minDistance: minDist,
 	}
+}
+
+func (kb *KBuckets) WhoAmI() *node.Node {
+	return kb.self
 }
 
 func (kb *KBuckets) BucketAtDistance(dist int) *Bucket {
