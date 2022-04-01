56b4a5430fe44c5f0f36042254847639d31050a1
melenium2 <osumelenium@gmail.com>
2022-04-01 09:58:29 +0500

    feat(table): add function for validating existed nodes

--- internal/table/table.go
+++ internal/table/table.go
@@ -2,8 +2,10 @@ package table
 
 import (
 	"context"
-	"crypto/rand"
+	crand "crypto/rand"
+	"math/rand"
 	"net"
+	"time"
 
 	"github.com/Melenium2/kademlia/internal/conn"
 	"github.com/Melenium2/kademlia/internal/kbuckets"
@@ -22,6 +24,8 @@ const (
 	NBuckets          = HashBits / 15       // Number of buckets.
 	BucketMinDistance = HashBits - NBuckets // Log distance of the closest bucket.
 
+	RefreshRate   = 1 * time.Minute
+	LiveCheckRate = 25 * time.Second
 )
 
 type Config struct {
@@ -82,12 +86,85 @@ func (t *Table) findNeighbors(node *node.Node) ([]*node.Node, error) {
 
 func (t *Table) findRandom() ([]*node.Node, error) {
 	var randID node.ID
-	_, _ = rand.Read(randID[:])
+	_, _ = crand.Read(randID[:])
 
 	n := node.NewNodeWithID(randID, &net.UDPAddr{})
 
 	return t.findNeighbors(n)
 }
+
+func (t *Table) Maintenance(ctx context.Context) {
+	var (
+		refreshTicker = time.NewTicker(RefreshRate)
+		pingTicker    = time.NewTicker(LiveCheckRate)
+	)
+
+	for {
+		select {
+		case <-refreshTicker.C:
+		case <-pingTicker.C:
+			go t.NodeValidation()
+		case <-ctx.Done():
+			refreshTicker.Stop()
+			pingTicker.Stop()
+
+			return
+		}
+	}
+}
+
+func (t *Table) NodeValidation() {
+	perm := rand.Perm(NBuckets)
+
+	var (
+		bucketIndex int
+		bucketNode  *node.Node
+	)
+
+	// find first not empty bucket.
+	for _, index := range perm {
+		bucket := t.buckets.BucketAtIndex(index)
+
+		if len(bucket.Entries) > 0 {
+			bucketNode = bucket.Entries[len(bucket.Entries)-1]
+			bucketIndex = index
+
+			break
+		}
+	}
+
+	if bucketNode == nil {
+		return
+	}
+
+	_, err := t.transport.SendPing(bucketNode)
+	if err != nil {
+		t.deleteLastNode(bucketIndex, bucketNode)
+
+		t.log.Errorf(
+			"can not send ping to node (%s) with addr %s, remove it from bucket %d",
+			bucketNode.ID(), bucketNode.Addr(), bucketIndex,
+		)
+
+		return
+	}
+
+	t.buckets.MoveFront(bucketIndex, bucketNode)
+}
+
+func (t *Table) deleteLastNode(bucketIndex int, node *node.Node) {
+	bucket := t.buckets.BucketAtIndex(bucketIndex)
+	last := bucket.Entries[len(bucket.Entries)-1]
+
+	// if bucket already empty, or last node not equals to provided node, return from func.
+	if len(bucket.Entries) == 0 || last.ID() != node.ID() {
+		return
+	}
+
+	t.buckets.Delete(bucketIndex, node)
+}
+
+func (t *Table) DiscoverNeighbors() {}
 
 // Docs
 // http://xlattice.sourceforge.net/components/protocol/kademlia/specs.html#implementation
