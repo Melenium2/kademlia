6a2b6c861cd1d5359f89a13e1c39ef4199ced2f5
melenium2 <osumelenium@gmail.com>
2022-04-12 07:46:31 +0500

    feat(dht): add example tests and doc

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Package kademlia implement Kademlia DHT protocol.
+//
+// Kademlia is a communications protocol for peer-to-peer networks.
+// It is one of many versions of a DHT, a Distributed Hash Table.
+package kademlia
---
+++ table_test.go
@@ -0,0 +1,47 @@
+package kademlia_test
+
+import (
+	"context"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/Melenium2/kademlia"
+)
+
+var (
+	addr, _ = net.ResolveUDPAddr("udp", ":1111")
+	conn, _ = net.ListenUDP("udp", addr)
+)
+
+var (
+	known1, _ = net.ResolveUDPAddr("udp", ":2222")
+	known2, _ = net.ResolveUDPAddr("udp", ":3333")
+	known3, _ = net.ResolveUDPAddr("udp", ":4444")
+	knwons    = []*net.UDPAddr{known1, known2, known3}
+)
+
+func TestDHT_Maintenance_Should_create_and_dht_table(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	dht := kademlia.New(conn, knwons)
+
+	dht.Maintenance(ctx)
+}
+
+func TestDHT_Maintenance_Should_create_new_dht_with_options(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	dht := kademlia.New(
+		conn,
+		knwons,
+		kademlia.WithLiveCheckRate(10*time.Second),
+		kademlia.WithTableRefreshRate(1*time.Minute),
+		kademlia.WithBucketSize(16),
+		kademlia.WithParallelCallsCount(3),
+	)
+
+	dht.Maintenance(ctx)
+}
