5593ad4f85f7677c9554f5883c2e6a003ca911d6
melenium2 <osumelenium@gmail.com>
2022-04-12 08:03:36 +0500

    chore(table): add comment

--- internal/table/table.go
+++ internal/table/table.go
@@ -18,6 +18,8 @@ import (
 //		Add client code for running table
 //		Add config variables
 //		Change encode/decode method
+//      Now we can not start out table without any bootstrap nodes,
+//		we should started without nodes too.
 
 const (
 	HashBits          = len(node.ID{}) * 8  // Length of hash in bits. Now this is length of SHA-1, 160 bits.
